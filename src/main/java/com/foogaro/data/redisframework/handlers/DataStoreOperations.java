9114626ae6e6b2322b93c7c87f9c68c09bdaeaa1
Foogaro <luigi@foogaro.com>
2022-03-30 12:47:54 +0200

    Methods now return T generic type and not Object.

--- src/main/java/com/foogaro/data/redisframework/handlers/DataStoreOperations.java
+++ src/main/java/com/foogaro/data/redisframework/handlers/DataStoreOperations.java
@@ -3,8 +3,8 @@ package com.foogaro.data.redisframework.handlers;
 public interface DataStoreOperations<T> {
 
     public boolean create(T payload);
-    public T read(Object id);
+    public <T> T read(String id, String jsonPath);
     public int update(T payload);
-    public int delete(Object id);
+    public int delete(String id);
 
 }
